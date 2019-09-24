e2958aff382421b5b5ab0e6ccb4ba74e7679d50b
adarsh-patil <adarsh-patil@looptelecom.com>
2019-09-24 15:00:53 +0800

    Added fibonacci using recursion

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,6 @@
 #Fibonacci series : To print n fibonacci numbers
  
+#Using loops
 def fibonacci(n):
     fib1,fib2=0,1
     for i in range (n):
@@ -9,4 +10,15 @@ def fibonacci(n):
         fib2=temp
         
 fibonacci(10)
+
+
+#Using recurssion
+def fibonacci(n):
+	if n<=1:
+		return n
+	else:
+		return (fibonacci(n-1)+fibonacci(n-2))
+nterms=int(input("Enter the no. of terms in series :"))
+for i in range (nterms):
+	print(fibonacci(i))
 
