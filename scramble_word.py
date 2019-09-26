8f1ba19525349a29274367a704df4342686ce2a1
patiladarsh94 <52648479+patiladarsh94@users.noreply.github.com>
2019-09-26 14:30:01 +0800

    Update scramble_word.py

--- scramble_word.py
+++ scramble_word.py
@@ -1,20 +1,32 @@
-import random
 
 #Note : "Srbmnacilg wrods is vrey itrensientg. Bscauee even if tehy are srelabcmd, it dosn'et ipcmat our raidneg. Bacusee we dn'ot raed lteetr by letetr, we raed the wrod as a wolhe."
 
+import random
+import re
 def scramble_word():
-	try:
-		f1=open("d:\\Learn\\test_scramble","r")
-		content=f1.read()
-		f2=open("d:\\Learn\\new_text","w+")
-		for str(word) in content:
-			temp_first=word[0]
-			temp_last=word[-1]
-			temp_str=word[1:-1].ascii_lowercase
-			new_str=temp_first+random.sample(temp_str,len(tmp_str))
-			f2.write(new_str)
-	except:
-		f1.close()
-	else:
-		f1.close()
-		f2.close()
+    f1 = open("d://Learn//test_scramble.txt", "r")
+    content = f1.read()
+    f2 = open("d://Learn//new_text.txt", "w+")
+    for word in content.split(" "):
+        if len(word)>3:
+            if re.search("^[A-Za-z]",word) !=None:
+                temp_first = word[0]
+                mid_start=1
+            else:
+                temp_first = word[:2]
+                mid_start = 2
+            if re.search("[A-Za-z]$",word) !=None:
+                temp_last = word[-1]
+                mid_last = -1
+            else:
+                temp_last=word[-2:]
+                mid_last = -2
+            temp_str = str(word[mid_start:mid_last]).lower()
+            new_str = str(temp_first)+"".join(random.sample(temp_str, len(temp_str)))+str(temp_last)+ " "
+        else:
+            new_str=str(word)+ " "
+
+        f2.write(new_str)
+    f1.close()
+    f2.close()
+scramble_word()
